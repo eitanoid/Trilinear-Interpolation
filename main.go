227730d270f6b8370ac004deb363e898d0684cb4
Eitan Ostashinski <eitanoid.os@gmail.com>
2024-12-30 21:39:58 +0000

    Cleaned up code

--- main.go
+++ main.go
@@ -12,19 +12,18 @@ import (
 func Linear_interp(start, end []float64, steps int) [][]float64 {
 
 	var dimension int = len(start) // the vector dimension, for RGBA colors this is 4
-
-	if len(start) != len(end) {
+	if len(start) != dimension {
 		panic("cannot interpolate between vectors of different dimensions")
 	}
 
 	diff := make([]float64, len(start))
-	for i := 0; i < len(start); i++ {
+	for i := 0; i < dimension; i++ {
 		diff[i] = (end[i] - start[i]) / float64(steps+1) //x .. x + i* diff ... x + steps*diff = y, we want the end length to be steps + 2, so we need diff to be the 1/(steps+1) fraction
 	}
 
 	//initialize the return variable
 	interp := make([][]float64, steps+2) // the length after interpolating steps times
-	interp[0] = start                    // we dont need to set end as it will be calculated.
+	interp[0] = start
 	interp[steps+1] = end
 
 	for i := 1; i < steps+1; i++ {
@@ -38,7 +37,7 @@ func Linear_interp(start, end []float64, steps int) [][]float64 {
 	return interp
 }
 
-// varialbes are the labeled corners as b= 0 , t= 1
+// varialbes are the labeled corners as b = 0 , t = 1 indexed as rowcol
 // bb bt
 // tb tt
 //
@@ -51,7 +50,7 @@ func Bilinear_interp(bb, bt, tb, tt []float64, steps int) [][][]float64 {
 		panic("cannot interpolate between vectors of different dimensions")
 	}
 
-	// initialize -- require unfortunately
+	// initialize
 	plane := make([][][]float64, steps+2) // [row][col][color] where bb is [0][0] and tt is [steps+2][steps+2]
 	for i := range plane {
 		plane[i] = make([][]float64, steps+2)
@@ -67,6 +66,26 @@ func Bilinear_interp(bb, bt, tb, tt []float64, steps int) [][][]float64 {
 	return plane
 }
 
+// looping order:
+// # is calculated @ is empty
+//
+// #@@@#
+// @@@@@
+// @@@@@
+// #@@@#
+//
+// this isn't actually done, but the top and bottom row interp is calculated.
+// #####
+// @@@@@
+// @@@@@
+// #####
+//
+// use the saved slices to linear interp each col, repeat this step for each col
+// #####
+// #@@@@
+// #@@@@
+// #####
+
 func Trilinear_interp(bbb, bbt, btb, btt, tbb, tbt, ttb, ttt []float64, steps int) [][][][]float64 { // height row col color
 
 	var dimension = len(bbb)
@@ -79,11 +98,11 @@ func Trilinear_interp(bbb, bbt, btb, btt, tbb, tbt, ttb, ttt []float64, steps in
 
 	cube := make([][][][]float64, steps+2)
 
-	cube[0] = Bilinear_interp(bbb, bbt, btb, btt, steps)
-	cube[steps+1] = Bilinear_interp(tbb, tbt, ttb, ttt, steps)
+	// fill in the top and bottom slices
+	// cube[0] = Bilinear_interp(bbb, bbt, btb, btt, steps)
+	// cube[steps+1] = Bilinear_interp(tbb, tbt, ttb, ttt, steps)
 
 	corners := make([][][]float64, steps+2) // [0,1,2,3 are bb bt tb tt], height, color
-
 	corners[0] = Linear_interp(bbb, tbb, steps)
 	corners[1] = Linear_interp(bbt, tbt, steps)
 	corners[2] = Linear_interp(btb, ttb, steps)
@@ -92,13 +111,39 @@ func Trilinear_interp(bbb, bbt, btb, btt, tbb, tbt, ttb, ttt []float64, steps in
 	// trilinear_interp
 	// iterate over the height of the cube and interpolate each slice using the current heights bb,bt,tb,tt
 
-	for i := 1; i < steps+1; i++ {
+	for i := 0; i < steps+2; i++ {
 		bb, bt, tb, tt := corners[0][i], corners[1][i], corners[2][i], corners[3][i]
 		cube[i] = Bilinear_interp(bb, bt, tb, tt, steps)
 	}
 
 	return cube
 }
+
+// looping order:
+// eg on a 4x4 cube
+// input:
+// 0     1     2     3
+// @##@  ####  ####  @##@
+// ####  ####  ####  ####
+// ####  ####  ####  ####
+// @##@  ####  ####  @##@
+//
+// calculate the corners
+//
+//  0     1     2     3
+//  @##@  @##@  @##@  @##@
+//  ####  ####  ####  ####
+//  ####  ####  ####  ####
+//  @##@  @##@  @##@  @##@
+//
+//  fill in the layers
+//
+//  0     1     2     3
+//  @@@@  @##@  @##@  @##@
+//  @@@@  ####  ####  ####
+//  @@@@  ####  ####  ####
+//  @@@@  @##@  @##@  @##@
+//  goes this way ->
 
 func main() {
 	// bb, bt, tb, tt := []float64{0, 0}, []float64{0, 10}, []float64{10, 0}, []float64{10, 20}
@@ -113,19 +158,12 @@ func main() {
 	// tbb, tbt, ttb, ttt := []float64{30, 100, 40, 255}, []float64{0, 100, 0, 255}, []float64{5, 50, 200, 255}, []float64{255, 100, 50, 255}
 	// cube := Trilinear_interp(bbb, bbt, btb, btt, tbb, tbt, ttb, ttt, 200)
 
-	corners := [][]float64{}
+	corners := [][]float64{} // generate random entries for this code
 	for i := 0; i < 8; i++ {
 		corners = append(corners, []float64{float64(rand.Intn(((i + 1) + 1) * 30)), float64(rand.Intn((i + 1) * 30)), float64(rand.Intn((i + 1) * 30)), 255})
 	}
 
-	cube := Trilinear_interp(corners[0], corners[1], corners[2], corners[3], corners[4], corners[5], corners[6], corners[7], 200)
-	// for i, slice := range cube {
-	// 	fmt.Printf("This is slice %d \n", i)
-	// 	for _, row := range slice {
-	// 		fmt.Println(row)
-	// 	}
-	// }
-
+	cube := Trilinear_interp(corners[0], corners[1], corners[2], corners[3], corners[4], corners[5], corners[6], corners[7], 20)
 	images := Export_Cube(cube)
 
 	for i, image := range images {
