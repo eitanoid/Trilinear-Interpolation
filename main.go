134cae9c9bd031e12f1103777f61385d4bc8047e
Eitan Ostashinski <eitanoid.os@gmail.com>
2025-01-01 17:20:03 +0000

    added more flags, now working on formatting terminal out

--- main.go
+++ main.go
@@ -13,9 +13,16 @@ type Vec []float64 // vector type
 var Supported_Formats = map[string]bool{"rgba": true, "oklab": true}
 
 func Print_Input(verts [2][2][2]Vec, depth int, format string) {
+	_depth := strconv.Itoa(depth)
 
-	front_face := map[string]string{"top": fmt.Sprintf("%s%s", verts[0][0][0].To_Ansi("000"), verts[0][0][1].To_Ansi("001")), "bottom": fmt.Sprintf("%s%s", verts[0][1][0].To_Ansi("010"), verts[0][1][1].To_Ansi("011"))}
-	back_face := map[string]string{"top": fmt.Sprintf("%s%s", verts[1][0][0].To_Ansi("100"), verts[1][0][1].To_Ansi("101")), "bottom": fmt.Sprintf("%s%s", verts[1][1][0].To_Ansi("110"), verts[1][1][1].To_Ansi("111"))}
+	front_face := map[string]string{
+		"top":    fmt.Sprintf("%s%s", verts[0][0][0].To_Ansi("000"), verts[0][0][1].To_Ansi("00"+_depth)),
+		"bottom": fmt.Sprintf("%s%s", verts[0][1][0].To_Ansi("010"), verts[0][1][1].To_Ansi("0"+_depth+_depth)),
+	}
+	back_face := map[string]string{
+		"top":    fmt.Sprintf("%s%s", verts[1][0][0].To_Ansi(_depth+"00"), verts[1][0][1].To_Ansi(_depth+"0"+_depth)),
+		"bottom": fmt.Sprintf("%s%s", verts[1][1][0].To_Ansi(_depth+_depth+"0"), verts[1][1][1].To_Ansi(_depth+_depth+_depth)),
+	}
 
 	fmt.Printf("Interpolating %d times, in %s format, between: \n%s   %s\n%s   %s\n", depth, format, front_face["top"], back_face["top"], front_face["bottom"], back_face["bottom"])
 
@@ -26,6 +33,7 @@ func main() {
 	_format := flag.String("format", "rgba", "In which format to interpolate.")
 	_depth := flag.Int("depth", 6, "Interpolate to 'depth' points.")
 	_verbose := flag.Bool("v", false, "Verbose")
+	_debug := flag.Bool("d", false, "Set vertecies to debug mode")
 	_generate_images := flag.Bool("i", false, "Generate 'depth' images of the interpolation")
 
 	flag.Parse()
@@ -34,6 +42,7 @@ func main() {
 	depth := *_depth
 	verbose := *_verbose
 	generate_images := *_generate_images
+	debug := *_debug
 
 	// fmt.Printf("%v,%v,%v,%v\n", format, depth, verbose, generate_images)
 
@@ -46,15 +55,17 @@ func main() {
 				255})
 	}
 
-	corners = []RGBA{ // constant rather than random values for debugging
-		{255, 255, 255, 255}, //#ffffff
-		{0, 0, 0, 255},       //#000000
-		{255, 0, 0, 255},     //#ff0000
-		{0, 255, 0, 255},     //#00ff00
-		{0, 0, 255, 255},     //#0000ff
-		{255, 0, 255, 255},   //#ff00ff
-		{255, 255, 0, 255},   //#ffff00
-		{0, 255, 255, 255},   //#00ffff
+	if debug {
+		corners = []RGBA{ // constant rather than random values for debugging
+			{255, 255, 255, 255}, //#ffffff
+			{0, 0, 0, 255},       //#000000
+			{255, 0, 0, 255},     //#ff0000
+			{0, 255, 0, 255},     //#00ff00
+			{0, 0, 255, 255},     //#0000ff
+			{255, 0, 255, 255},   //#ff00ff
+			{255, 255, 0, 255},   //#ffff00
+			{0, 255, 255, 255},   //#00ffff
+		}
 	}
 
 	// verts is indexed as [forward 0 / backward 1][top 0 / bottom 1][left 0/ right 1]
